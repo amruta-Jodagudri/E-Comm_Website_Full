d6cf45de7fa7feb673711abe39046da49bba6bd4
Amruta Jodagudri <144842080+amruta-Jodagudri@users.noreply.github.com>
2024-06-18 14:41:50 +0530

    Done

--- src/components/frontend/ProductCard.tsx
+++ src/components/frontend/ProductCard.tsx
@@ -30,7 +30,7 @@ const ProductCard = ({id,img,category,title,price}:PropsType)=>{
     return(
         <div className="border border-gray-200">
             <div className="text-center border-b border-gray-200">
-                <img src={img} alt={title} className="inline-block" />
+                <img src={img} width={400} height={400} alt={title} className="inline-block" />
             </div>
 
             <div className="px-8 py-4">
@@ -46,7 +46,7 @@ const ProductCard = ({id,img,category,title,price}:PropsType)=>{
                     <p className="text-gray-600 text-[14px] ml-2">(3 Review)</p>
                 </div>
 
-                <div className="flex justify-between items center mt-4">
+                <div className="flex justify-between items-center mt-4">
                     <h2 className="font-medium text-accept text-xl">${price}</h2>
                     <div className="flex gap-2 items-center bg-pink text-white px-4 py-2 cursor-pointer hover:bg-accept"
                     onClick={addProductTocart}>
